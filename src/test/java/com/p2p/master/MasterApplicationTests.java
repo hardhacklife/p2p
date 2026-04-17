f27b2a758b475b158baecbcf34bdecc1a511a5d3
hardhacklife <abourabbere@gmail.com>
2026-04-17 18:08:13 +0000

    first commit

---
+++ src/test/java/com/p2p/master/MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
